fb180bea934484c6f55dd877fce564c19a1c7e5e
Максим <99335487+DanteVinHack@users.noreply.github.com>
2022-10-22 16:45:47 +0300

    Получение данных о билетах

---
+++ Билеты на событие/src/js/app.js
@@ -0,0 +1,30 @@
+import '../scss/style.scss'
+import createTable from './create-table.js';
+import tickets from '../assets/tickets.json';
+
+// Здесь мы имитируем получение данных, 
+// но для получения настоящий данных могли бы использовать
+// библиотеку "Axios" или же fetch запросы встроенные в браузер
+const getTickets = async () => {
+  const response = await new Promise(res => {
+    // Имитируем задержку
+    setTimeout(() => {
+      res(tickets)
+    }, 1069)
+  });
+
+  // Изменяем данные по условию задачи
+  const ticketsData = response.map(ticket => ({
+    ...ticket,
+    equal_price: ticket.type === 'benefits' ? 
+    0 : ticket.type === 'group' ? 
+    Math.round(ticket.equal_price / 3) : ticket.equal_price,
+  }));
+
+  const table = createTable(ticketsData)
+
+  document.body.append(table)
+
+}
+
+getTickets()
